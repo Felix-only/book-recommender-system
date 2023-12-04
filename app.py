bc2f2d45ed7ccebe93d073f5822cd52a23e8d0f9
Felix Huang <felixhuang101@gmail.com>
2023-12-04 01:34:43 -0500

    add nav in app.py and added images

--- app.py
+++ app.py
@@ -2,68 +2,81 @@ import pickle
 import streamlit as st
 import numpy as np
 
-st.header("book")
-model = pickle.load(open('py_objects/model.pkl','rb'))
-book_names = pickle.load(open('py_objects/books_name.pkl','rb'))
-final_rating = pickle.load(open('py_objects/final_rating.pkl','rb'))
-book_pivot = pickle.load(open('py_objects/pivot.pkl','rb'))
+nav = st.sidebar.radio("Navigation", ['Home', 'Book Recommender'])
 
-selected_books = st.selectbox(
-    "Type or select a book",
-    book_names
-)
-
-def fetch_poster(suggestion):
-    book_name = []
-    ids_index = []
-    poster_url = []
-
-    for book_id in suggestion:
-        book_name.append(book_pivot.index[book_id])
-
-    for name in book_name[0]: 
-        ids = np.where(final_rating['title'] == name)[0][0]
-        ids_index.append(ids)
-
-    for idx in ids_index:
-        url = final_rating.iloc[idx]['Image_URL']
-        poster_url.append(url)
-
-    return poster_url
-
-def recommend_book(book_name):
-    book_list = []
-    book_id = np.where(book_pivot.index == book_name)[0][0]
-    # you could appoint n-1 recommendation here
-    distance, suggestion = model.kneighbors(book_pivot.iloc[book_id].values.reshape(1,-1), n_neighbors=6)
+if nav == 'Home':
+    st.title('My title')
+    st.image('./images/book_recommender.png', width = 300)
     
-    poster_url = fetch_poster(suggestion)
+    st.text(' ')
+    st.text(' ')
+    st.text('Fixed width text')
+    st.code('''for i in range(8):
+    print('Hello World')''')
 
-    for i in range(len(suggestion[0])):
-        book_list.append(book_pivot.index[suggestion[0][i]])
+if nav == 'Book Recommender':
+    st.header("Book Recommender")
+    model = pickle.load(open('py_objects/model.pkl','rb'))
+    book_names = pickle.load(open('py_objects/books_name.pkl','rb'))
+    final_rating = pickle.load(open('py_objects/final_rating.pkl','rb'))
+    book_pivot = pickle.load(open('py_objects/pivot.pkl','rb'))
 
-    return book_list, poster_url
+    selected_books = st.selectbox(
+        "Type or select a book",
+        book_names
+    )
 
-if st.button('show recommendation'):
-    recommendation_books, poster_url = recommend_book(selected_books)
-    col1, col2, col3, col4, col5 = st.columns(5)
+    def fetch_poster(suggestion):
+        book_name = []
+        ids_index = []
+        poster_url = []
 
-    with col1:
-        st.text(recommendation_books[1])
-        st.image(poster_url[1])
+        for book_id in suggestion:
+            book_name.append(book_pivot.index[book_id])
 
-    with col2:
-        st.text(recommendation_books[2])
-        st.image(poster_url[2])
-    
-    with col3:
-        st.text(recommendation_books[3])
-        st.image(poster_url[3])
+        for name in book_name[0]: 
+            ids = np.where(final_rating['title'] == name)[0][0]
+            ids_index.append(ids)
 
-    with col4:
-        st.text(recommendation_books[4])
-        st.image(poster_url[4])
+        for idx in ids_index:
+            url = final_rating.iloc[idx]['Image_URL']
+            poster_url.append(url)
 
-    with col5:
-        st.text(recommendation_books[5])
-        st.image(poster_url[5])
+        return poster_url
+
+    def recommend_book(book_name):
+        book_list = []
+        book_id = np.where(book_pivot.index == book_name)[0][0]
+        # you could appoint n-1 recommendation here
+        distance, suggestion = model.kneighbors(book_pivot.iloc[book_id].values.reshape(1,-1), n_neighbors=6)
+        
+        poster_url = fetch_poster(suggestion)
+
+        for i in range(len(suggestion[0])):
+            book_list.append(book_pivot.index[suggestion[0][i]])
+
+        return book_list, poster_url
+
+    if st.button('show recommendation'):
+        recommendation_books, poster_url = recommend_book(selected_books)
+        col1, col2, col3, col4, col5 = st.columns(5)
+
+        with col1:
+            st.text(recommendation_books[1])
+            st.image(poster_url[1])
+
+        with col2:
+            st.text(recommendation_books[2])
+            st.image(poster_url[2])
+        
+        with col3:
+            st.text(recommendation_books[3])
+            st.image(poster_url[3])
+
+        with col4:
+            st.text(recommendation_books[4])
+            st.image(poster_url[4])
+
+        with col5:
+            st.text(recommendation_books[5])
+            st.image(poster_url[5])
