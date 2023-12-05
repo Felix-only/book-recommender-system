de17c097d576dac6a18043c2e7e2f07beab229d4
Bella Liang <bellal@BelladeMBP-5.fios-router.home>
2023-12-05 13:12:28 -0500

    finish visualization 1.0

--- app.py
+++ app.py
@@ -141,121 +141,171 @@ if nav == 'Book Recommender':
     
 #visualization page
 if nav == 'Visualization':
-    st.header('Data Visualization')
-    fig = plt.figure()
-    st.text(' ')
-    st.markdown('**Chcek out the distribution of Data**')
-    st.text(' ')
-    if st.button('Distribution of Book Ratings'):
-        # Create a figure for Matplotlib
-        fig, ax = plt.subplots()
+    tab1, tab2, tab3 = st.tabs(["Ratings & Age Distribution", "top rated books (with age)", "top rated books based on regions"])
 
-        # Create the histogram with a log scale for the y-axis
-        sns.histplot(ratings['Book-Rating'], kde=False, bins=30, ax=ax)
-        ax.set_yscale('log')  # Sets a logarithmic scale on the y-axis
-        ax.set(xlabel='Book Rating', ylabel='Log Count')  # Labels the axes
+    with tab1:
 
-        # Adds a KDE plot on top of the histogram with the same x-axis
-        sns.kdeplot(ratings['Book-Rating'], ax=ax, color='red')
+        st.header('Data Visualization')
+        st.text(' ')
+        st.markdown('**Chcek out the distribution of Data**')
+        st.text(' ')
+        if st.button('Distribution of Book Ratings'):
+            # Create a figure for Matplotlib
+            fig, ax = plt.subplots()
 
-        # Sets the title for the plot
-        ax.set_title('Distribution of Book Ratings')
+            # Create the histogram with a log scale for the y-axis
+            sns.histplot(ratings['Book-Rating'], kde=False, bins=30, ax=ax)
+            ax.set_yscale('log')  # Sets a logarithmic scale on the y-axis
+            ax.set(xlabel='Book Rating', ylabel='Log Count')  # Labels the axes
 
-        # Display the plot in the Streamlit app
+            # Adds a KDE plot on top of the histogram with the same x-axis
+            sns.kdeplot(ratings['Book-Rating'], ax=ax, color='red')
+
+            # Sets the title for the plot
+            ax.set_title('Distribution of Book Ratings')
+
+            # Display the plot in the Streamlit app
+            st.pyplot(fig)
+
+        if st.button('Age Distribution of Users'):
+            # Create a figure for Matplotlib
+            fig, ax = plt.subplots()
+
+            # Create the histogram and KDE plot
+            sns.histplot(users['Age'], kde=True)
+            ax.set_xlim(5, 80)  # Sets the x-axis limit from 5 to 80
+            ax.set_xlabel('Age')  # Sets the x-axis label
+            ax.set_ylabel('Count')  # Sets the y-axis label
+            ax.set_title('Age Distribution of Users')  # Sets the title of the plot
+
+            # Display the plot in the Streamlit app
+            st.pyplot(fig)
+
+    with tab2:
+
+        st.header('Top Rated books')
+        # Count the number of ratings for each book
+        book_rating_counts = ratings['ISBN'].value_counts().reset_index()
+        book_rating_counts.columns = ['ISBN', 'Rating-Count']
+
+        # Filter for books with 50 or more ratings
+        books_with_50plus_ratings = book_rating_counts[book_rating_counts['Rating-Count'] >= 50]
+
+        # Merge this filtered dataset with the original ratings and then with the books dataset
+        ratings_50plus_df = pd.merge(books_with_50plus_ratings, ratings, on="ISBN")
+        ratings_books_50plus_df = pd.merge(ratings_50plus_df, books, on="ISBN")
+
+        # Set the aesthetic style of the plots
+        sns.set_style("whitegrid")
+
+        num = st.slider('Top Rated Books (with at least 50 ratings)', 0, 20, 5)
+        # Top num rated books with 50+ ratings
+        top_rated_books_50plus_df = ratings_books_50plus_df.groupby('Book-Title')['Book-Rating'].mean().reset_index()
+        top_rated_books_50plus_df = top_rated_books_50plus_df.sort_values('Book-Rating', ascending=False).head(num)
+
+        # Visualization for Top num Rated Books and Their Ratings
+        fig = plt.figure(figsize=(10, num + 1))
+        chart = sns.barplot(x='Book-Rating', y='Book-Title', data=top_rated_books_50plus_df, palette='coolwarm')
+        plt.title('Top '+ str(num) + ' Rated Books (with at least 50 ratings)')
+        plt.xlabel('Average Rating')
+        plt.ylabel('Book Title')
+
+        # Adding the text on the bars
+        for p in chart.patches:
+            width = p.get_width()
+            plt.text(p.get_width(), p.get_y() + p.get_height()/2. + 0.1, '{:1.2f}'.format(width), ha="left")
+        
         st.pyplot(fig)
 
-    if st.button('Age Distribution of Users'):
-        # sns.histplot(users['Age'], kde=True)
-        # st.pyplot(fig)
 
-        # Create a figure for Matplotlib
-        fig, ax = plt.subplots()
+        age_young = st.slider('Top Rated Books with Younger Users (with at least 50 ratings)', 0, 20, 5)
 
-        # Create the histogram and KDE plot
-        sns.histplot(users['Age'], kde=True)
-        ax.set_xlim(5, 80)  # Sets the x-axis limit from 5 to 80
-        ax.set_xlabel('Age')  # Sets the x-axis label
-        ax.set_ylabel('Count')  # Sets the y-axis label
-        ax.set_title('Age Distribution of Users')  # Sets the title of the plot
+        # Calculate the average age of users for each book with 50+ ratings
+        ratings_books_users_50plus_df = pd.merge(ratings_books_50plus_df, users, on="User-ID")
+        books_avg_age_50plus_df = ratings_books_users_50plus_df.groupby('Book-Title')['Age'].mean().reset_index()
 
-        # Display the plot in the Streamlit app
-        st.pyplot(fig)
+        # Top books with the youngest average user age
+        top_5_youngest_books_50plus_df = books_avg_age_50plus_df.sort_values('Age').head(age_young)
+
+        # Visualization for Top Books with the Youngest Average User Age
+        fig2 = plt.figure(figsize=(10, age_young + 1))
+        chart = sns.barplot(x='Age', y='Book-Title', data=top_5_youngest_books_50plus_df, palette='mako')
+        plt.title('Top ' + str(age_young) + ' Books with the Youngest Average User Age (with at least 50 ratings)')
+        plt.xlabel('Average Age of Users')
+        plt.ylabel('Book Title')
+
+        # Adding the text on the bars
+        for p in chart.patches:
+            width = p.get_width()
+            plt.text(p.get_width(), p.get_y() + p.get_height()/2. + 0.1, '{:1.2f}'.format(width), ha="left")
+
+        st.pyplot(fig2)
 
 
-    st.header('Top Rated books')
-    # Count the number of ratings for each book
-    book_rating_counts = ratings['ISBN'].value_counts().reset_index()
-    book_rating_counts.columns = ['ISBN', 'Rating-Count']
+        age_old = st.slider('Top Rated Books with Older Users (with at least 50 ratings)', 0, 20, 5)
+        
+        # Top books with the oldest average user age
+        top_5_oldest_books_50plus_df = books_avg_age_50plus_df.sort_values('Age', ascending=False).head(age_old)
 
-    # Filter for books with 50 or more ratings
-    books_with_50plus_ratings = book_rating_counts[book_rating_counts['Rating-Count'] >= 50]
+        # Visualization for Top Books with the Oldest Average User Age
+        fig3 = plt.figure(figsize=(10, age_old + 1))
+        chart = sns.barplot(x='Age', y='Book-Title', data=top_5_oldest_books_50plus_df, palette='copper')
+        plt.title('Top ' + str(age_old) + ' Books with the Oldest Average User Age (with at least 50 ratings)')
+        plt.xlabel('Average Age of Users')
+        plt.ylabel('Book Title')
 
-    # Merge this filtered dataset with the original ratings and then with the books dataset
-    ratings_50plus_df = pd.merge(books_with_50plus_ratings, ratings, on="ISBN")
-    ratings_books_50plus_df = pd.merge(ratings_50plus_df, books, on="ISBN")
+        # Adding the text on the bars
+        for p in chart.patches:
+            width = p.get_width()
+            plt.text(p.get_width(), p.get_y() + p.get_height()/2. + 0.1, '{:1.2f}'.format(width), ha="left")
 
-    # Set the aesthetic style of the plots
-    sns.set_style("whitegrid")
+        st.pyplot(fig3)
 
-    num = st.slider('Top Rated Books (with at least 50 ratings)', 0, 20, 5)
-    # Top num rated books with 50+ ratings
-    top_rated_books_50plus_df = ratings_books_50plus_df.groupby('Book-Title')['Book-Rating'].mean().reset_index()
-    top_rated_books_50plus_df = top_rated_books_50plus_df.sort_values('Book-Rating', ascending=False).head(num)
+    with tab3:
 
-    # Visualization for Top num Rated Books and Their Ratings
-    fig = plt.figure(figsize=(10, num + 1))
-    chart = sns.barplot(x='Book-Rating', y='Book-Title', data=top_rated_books_50plus_df, palette='coolwarm')
-    plt.title('Top '+ str(num) + ' Rated Books (with at least 50 ratings)')
-    plt.xlabel('Average Rating')
-    plt.ylabel('Book Title')
+        st.header('Top rated books in specific regions')
 
-    # Adding the text on the bars
-    for p in chart.patches:
-        width = p.get_width()
-        plt.text(p.get_width(), p.get_y() + p.get_height()/2. + 0.1, '{:1.2f}'.format(width), ha="left")
+        with open('py_objects/region_unique.pkl', 'rb') as file:
+            region_unique = pickle.load(file)
     
-    st.pyplot(fig)
+        selected_region = st.selectbox(
+            "Please select the region",
+            region_unique
+        )
+        
+        num_top = st.slider('please select the number of books', 0, 10, 5)
 
+        # Filter users from the US and the UK
+        users_us = users[users['Location'].str.contains(selected_region, case=False, na=False)]
 
-    age_young = st.slider('Top Rated Books with Younger Users (with at least 50 ratings)', 0, 20, 5)
+        # Merge the filtered users with the ratings
+        ratings_us = pd.merge(users_us, ratings, on="User-ID")
 
-    # Calculate the average age of users for each book with 50+ ratings
-    ratings_books_users_50plus_df = pd.merge(ratings_books_50plus_df, users, on="User-ID")
-    books_avg_age_50plus_df = ratings_books_users_50plus_df.groupby('Book-Title')['Age'].mean().reset_index()
+        # Merge this with the books data
+        ratings_books_us = pd.merge(ratings_us, books, on="ISBN")
 
-    # Top books with the youngest average user age
-    top_5_youngest_books_50plus_df = books_avg_age_50plus_df.sort_values('Age').head(age_young)
+        # Count the number of ratings for each book in the US and the UK
+        book_rating_counts_us = ratings_books_us['ISBN'].value_counts().reset_index()
+        book_rating_counts_us.columns = ['ISBN', 'Rating-Count']
 
-    # Visualization for Top Books with the Youngest Average User Age
-    fig2 = plt.figure(figsize=(10, age_young + 1))
-    chart = sns.barplot(x='Age', y='Book-Title', data=top_5_youngest_books_50plus_df, palette='mako')
-    plt.title('Top ' + str(age_young) + ' Books with the Youngest Average User Age (with at least 50 ratings)')
-    plt.xlabel('Average Age of Users')
-    plt.ylabel('Book Title')
+        # Merge this filtered dataset with the original ratings and then with the books dataset
+        ratings_books_50plus_us = pd.merge(book_rating_counts_us, ratings_books_us, on="ISBN")
 
-    # Adding the text on the bars
-    for p in chart.patches:
-        width = p.get_width()
-        plt.text(p.get_width(), p.get_y() + p.get_height()/2. + 0.1, '{:1.2f}'.format(width), ha="left")
+        # Top rated books in the US and the UK with 50+ ratings
+        top_rated_books_50plus_us = ratings_books_50plus_us.groupby('Book-Title')['Book-Rating'].mean().reset_index()
+        top_rated_books_50plus_us = top_rated_books_50plus_us.sort_values('Book-Rating', ascending=False).head(num_top)
 
-    st.pyplot(fig2)
+        # Visualization for Top Rated Books in the US and the UK with 50+ ratings
+        fig4 = plt.figure(figsize=(10, num_top + 1))
+        chart = sns.barplot(x='Book-Rating', y='Book-Title', data=top_rated_books_50plus_us, palette='coolwarm')
+        plt.title('Top ' + str(num_top) + ' Rated Books in the ' + str(selected_region))
+        plt.xlabel('Average Rating')
+        plt.ylabel('Book Title')
 
-
-    age_old = st.slider('Top Rated Books with Older Users (with at least 50 ratings)', 0, 20, 5)
+        # Adding the text on the bars
+        for p in chart.patches:
+            width = p.get_width()
+            plt.text(p.get_width(), p.get_y() + p.get_height()/2. + 0.2, '{:1.2f}'.format(width), ha="left")
+        
+        st.pyplot(fig4)
     
-    # Top books with the oldest average user age
-    top_5_oldest_books_50plus_df = books_avg_age_50plus_df.sort_values('Age', ascending=False).head(age_old)
-
-    # Visualization for Top Books with the Oldest Average User Age
-    fig3 = plt.figure(figsize=(10, age_old + 1))
-    chart = sns.barplot(x='Age', y='Book-Title', data=top_5_oldest_books_50plus_df, palette='copper')
-    plt.title('Top ' + str(age_old) + ' Books with the Oldest Average User Age (with at least 50 ratings)')
-    plt.xlabel('Average Age of Users')
-    plt.ylabel('Book Title')
-
-    # Adding the text on the bars
-    for p in chart.patches:
-        width = p.get_width()
-        plt.text(p.get_width(), p.get_y() + p.get_height()/2. + 0.1, '{:1.2f}'.format(width), ha="left")
-
-    st.pyplot(fig3)
